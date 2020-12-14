8c7b03c10a250c63d8cbaf21e9e8efb300725dcd
Rob Dickinson <robfromboulder@gmail.com>
2020-12-14 02:13:16 -0700

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# transformer
-Clean or enhance ndjson files
+# resurfaceio-transformer
+
+## Supported file formats
+
+* .ndjson - Newline Delimited JSON (http://ndjson.org)
+* .ndjson.gz - Newline Delimited JSON with GZIP compression
+
+Each line in the input file is parsed as a Resurface message:
+https://resurface.io/json.html
+
+## System requirements
+
+* Java 8 or 11
+* Maven
+
+## Building from sources
+
+```
+git clone https://github.com/resurfaceio/transformer.git resurfaceio-transformer
+cd resurfaceio-transformer
+mvn package
+```
+
+## Transforming local file
+
+```
+FILE_IN=~/Dropbox/datasets/website.ndjson.gz FILE_OUT=~/Downloads/test.ndjson.gz java -Xmx192M -jar target/main-jar-with-dependencies.jar
+```
---
+++ src/main/java/io/resurface/transformer/Main.java
@@ -0,0 +1,70 @@
+// © 2016-2020 Resurface Labs Inc.
+
+package io.resurface.transformer;
+
+import io.resurface.messages.HttpMessage;
+import io.resurface.messages.MessageFileReader;
+import io.resurface.messages.MessageFileWriter;
+
+/**
+ * Transforms messages stored in compressed NDJSON files.
+ */
+public class Main {
+
+    /**
+     * Runs transformer as command-line program.
+     */
+    public static void main(String[] args) throws Exception {
+        System.out.println("\n>>> Transformer starting");
+        new Main();
+        System.out.println(">>> Transformer finished!\n");
+    }
+
+    /**
+     * Parses every message and writes a transformed version.
+     */
+    public Main() {
+        // read configuration
+        String file_in = System.getenv("FILE_IN");
+        if (file_in == null) throw new IllegalArgumentException("Missing FILE_IN");
+        System.out.println("FILE_IN=" + file_in);
+        String file_out = System.getenv("FILE_OUT");
+        if (file_out == null) throw new IllegalArgumentException("Missing FILE_OUT");
+        System.out.println("FILE_OUT=" + file_out);
+
+        // transform all messages
+        try (MessageFileWriter writer = new MessageFileWriter(file_out)) {
+            try (MessageFileReader reader = new MessageFileReader(file_in)) {
+                reader.parse((HttpMessage message) -> {
+                    transform(message);
+                    writer.write(message);
+                    if (messages_written++ % 100 == 0) status();
+                });
+            }
+        }
+
+        status();  // show final status
+    }
+
+    /**
+     * Transform message before writing.
+     */
+    private void transform(HttpMessage message) {
+        // add interval if none exists
+        if (message.interval_millis() == 0) message.set_interval_millis((int) (Math.random() * 15000));
+    }
+
+    /**
+     * Print status summary.
+     */
+    private void status() {
+        long elapsed = System.currentTimeMillis() - started;
+        long rate = (messages_written * 1000 / elapsed);
+        System.out.println("Messages: " + messages_written + ", Elapsed time: " + elapsed
+                + " ms, Rate: " + rate + " msg/sec");
+    }
+
+    private long messages_written = 0;
+    private final long started = System.currentTimeMillis();
+
+}
