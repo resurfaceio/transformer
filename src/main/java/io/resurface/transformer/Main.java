d70510ed8aedc7bae75120cefac0d48aabacb68b
Rob Dickinson <robfromboulder@gmail.com>
2023-09-15 16:56:31 -0600

    Deduplicate calls

--- src/main/java/io/resurface/transformer/Main.java
+++ src/main/java/io/resurface/transformer/Main.java
@@ -6,6 +6,10 @@ import io.resurface.ndjson.HttpMessage;
 import io.resurface.ndjson.MessageFileReader;
 import io.resurface.ndjson.MessageFileWriter;
 
+import java.security.MessageDigest;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Transforms messages stored in compressed NDJSON files.
  */
@@ -14,7 +18,7 @@ public class Main {
     /**
      * Runs transformer as command-line program.
      */
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("\n>>> Transformer starting");
         new Main();
         System.out.println(">>> Transformer finished!\n");
@@ -36,9 +40,11 @@ public class Main {
         try (MessageFileWriter writer = new MessageFileWriter(file_out)) {
             try (MessageFileReader reader = new MessageFileReader(file_in)) {
                 reader.parse((HttpMessage message) -> {
-                    transform(message);
-                    writer.write(message);
-                    if (messages_written++ % 100 == 0) status();
+                    messages_read++;
+                    if (transform(message)) {
+                        writer.write(message);
+                        if (messages_written++ % 1000 == 0) status();
+                    }
                 });
             }
         }
@@ -47,14 +53,73 @@ public class Main {
     }
 
     /**
-     * Transform message before writing.
+     * Transform message before writing, returning false if message should not be written.
      */
-    private void transform(HttpMessage message) {
-        // add interval if none exists
-        // if (message.interval_millis() == 0) message.set_interval_millis((int) (Math.random() * 15000));
+    private boolean transform(HttpMessage message) {
+        try {
+            // calculate digest for specified message
+            MessageDigest d = MessageDigest.getInstance("SHA-256");
+            digestUpdate(d, message.host());
+            digestUpdate(d, message.interval_millis());
+            digestUpdate(d, message.request_body());
+            digestUpdate(d, message.request_content_type());
+            digestUpdate(d, message.request_headers_json());
+            digestUpdate(d, message.request_method());
+            digestUpdate(d, message.request_params_json());
+            digestUpdate(d, message.request_url());
+            digestUpdate(d, message.request_user_agent());
+            digestUpdate(d, message.response_body());
+            digestUpdate(d, message.response_code());
+            digestUpdate(d, message.response_content_type());
+            digestUpdate(d, message.response_headers_json());
+            digestUpdate(d, message.response_time_millis());
+            digestUpdate(d, message.custom_fields_json());
+            digestUpdate(d, message.request_address());
+            digestUpdate(d, message.session_fields_json());
 
-        // reset response time to now
-        message.set_response_time_millis(0);
+            // skip processing for any duplicates detected
+            String digest = bytesToHex(d.digest());
+            if (hashes.contains(digest)) return false;
+            hashes.add(digest);
+
+            // add interval if none exists
+            if (message.interval_millis() == 0) message.set_interval_millis((int) (Math.random() * 15000));
+
+            // reset response time to now
+            message.set_response_time_millis(0);
+
+            return true;
+        } catch (Throwable t) {
+            System.out.println(t.getMessage());
+            return false;
+        }
+    }
+
+    /**
+     * Returns string representation of hash digest.
+     */
+    private static String bytesToHex(byte[] digest) {
+        StringBuilder s = new StringBuilder(2 * digest.length);
+        for (byte b : digest) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) s.append('0');
+            s.append(hex);
+        }
+        return s.toString();
+    }
+
+    /**
+     * Updates hash digest for specified string value.
+     */
+    private static void digestUpdate(MessageDigest digest, String value) {
+        digest.update(value == null ? "".getBytes() : value.getBytes());
+    }
+
+    /**
+     * Update hash digest for specified long value.
+     */
+    private static void digestUpdate(MessageDigest digest, long value) {
+        digestUpdate(digest, value == 0 ? "" : String.valueOf(value));
     }
 
     /**
@@ -62,13 +127,13 @@ public class Main {
      */
     private void status() {
         long elapsed = System.currentTimeMillis() - started;
-        long rate = (messages_written * 1000 / elapsed);
-        System.out.println("Messages: " + messages_written + ", Elapsed time: " + elapsed + " ms, Rate: " + rate + " msg/sec");
+        long rate = (messages_read * 1000 / elapsed);
+        System.out.println("Messages read: " + messages_read + ", messages written: " + messages_written + ", Elapsed time: " + elapsed + " ms, Rate: " + rate + " msg/sec");
     }
 
+    final private Set<String> hashes = new HashSet<>();
+    private long messages_read = 0;
     private long messages_written = 0;
     private final long started = System.currentTimeMillis();
-    private final long millis_per_year = 365L * 24 * 60 * 60 * 1000;
-    private final long a_year_ago = started - millis_per_year;
 
 }
